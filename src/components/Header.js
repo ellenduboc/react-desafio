8ffa7043794aa1d8dd13b8db5dc2a2f2fa3ec473
Ksouza <ksouza@BR0223.local>
2020-03-17 15:01:57 -0300

    changes to overall styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
 function Header() {
   return (
-    <div className="header" >
-      <h1 className="title" >Facebook</h1>
-      <h2 className="profile" >Meu perfil</h2>
-      <img src={require("../assets/perfil.png")} />
+    <div className="header">
+      <h1 className="title">Facebook</h1>
+      <h2 className="profile">Meu perfil</h2>
     </div>
-  )
-};
+  );
+}
 
 export default Header;
-
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import '../App.css';
-
+import React from "react";
+import "../App.css";
 
 class Questions extends React.Component {
   state = {
@@ -20,39 +19,47 @@ class Questions extends React.Component {
               name: "Nara Neres",
               avatar: ""
             },
-            content: "bla bla bla",
+            content: "bla bla bla"
           }
         ]
       }
     ]
   };
 
-
   render() {
     return (
-      <div className="center" className="container" >
+      <div className="center" className="container">
         <div>
           <div>
             <img className="areaimg" src={require("../assets/user1.png")} />
             <div>
-              <span className="user1" >Ellen Duboc</span>
+              <span className="user1">Ellen Duboc</span>
               <br />
-              <p className="date" >16/03/2020</p>
+              <p className="date">16/03/2020</p>
             </div>
           </div>
-          <p className="quest" >Porque o Coronavírus é um problema tão grande?</p>
-          <div>
+          <p className="quest">
+            Porque o Coronavírus é um problema tão grande?
+          </p>
+          <div className="comments-section">
             <img className="areaimg" src={require("../assets/user1.png")} />
-            <div className="wrapper" >
-              <div className="margincomment">
-                <span className="user2 ">Nara Neres</span>
-                <span className="comment2">O principal problema do coronavírus não é, de fato, a sua taxa de mortalidade, mas sim a sua alta taxa de transmissão, o que vai superlotar o sistema de saúde. A taxa de hospitalização do coronavírus é cerca de 20% e o número de pessoas que precisam de UTI é cerca de 5%. Por exemplo, se a Itália, mantiver o mesmo nível de transmissão da doença, no dia 24/03 terão 288000 casos de coronavírus no país. Se 5% dessas 288 mil pessoas precisarem de UTI, então serão 14400 pessoas necessitando de UTI.  </span>
-              </div>
+            <div className="wrapper">
+              <span className="user2 ">Nara Neres</span>
+              <span className="comment2">
+                O principal problema do coronavírus não é, de fato, a sua taxa
+                de mortalidade, mas sim a sua alta taxa de transmissão, o que
+                vai superlotar o sistema de saúde. A taxa de hospitalização do
+                coronavírus é cerca de 20% e o número de pessoas que precisam de
+                UTI é cerca de 5%. Por exemplo, se a Itália, mantiver o mesmo
+                nível de transmissão da doença, no dia 24/03 terão 288000 casos
+                de coronavírus no país. Se 5% dessas 288 mil pessoas precisarem
+                de UTI, então serão 14400 pessoas necessitando de UTI.{" "}
+              </span>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
